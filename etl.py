8ba8b2bc3b2ce2fb15c6cebee2a47ecfa426f7f0
JoseEslava <32904288+JoseEslava@users.noreply.github.com>
2021-02-02 13:44:23 -0600

    Add files via upload

--- etl.py
+++ etl.py
@@ -4,12 +4,18 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    """
+    This function loads data into staging tables that are necessary for collect all raw data, without filters, in a temporary storage. This tables have the data for create a star schema
+    """
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    """
+    This functions populate star schema tables, applying filters and using permanent storage. The main filter is page = 'NextSong'
+    """
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
--- sql_queries.py
+++ sql_queries.py
@@ -7,7 +7,7 @@ config.read('dwh.cfg')
 # DROP TABLES
 staging_events_table_drop = "DROP TABLE IF EXISTS staging_events"
 staging_songs_table_drop = "DROP TABLE IF EXISTS staging_songs"
-songplay_table_drop = "DROP TABLE IF EXISTS songplay"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
@@ -16,7 +16,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 staging_events_table_create= ("""
-CREATE TABLE IF NOT EXISTS staging_events(
+CREATE TABLE IF NOT EXISTS staging_events (
 artist varchar,
 auth varchar,
 firstName varchar,
@@ -39,7 +39,7 @@ userId int
 """)
 
 staging_songs_table_create = ("""
-CREATE TABLE IF NOT EXISTS staging_songs(
+CREATE TABLE IF NOT EXISTS staging_songs (
 num_songs int,
 artist_id varchar,
 artist_latitude float,
@@ -48,13 +48,13 @@ artist_location varchar,
 artist_name varchar,
 song_id varchar,
 title varchar,
-duration float,
+duration numeric,
 year int
 )
 """)
 
 songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS songplay (
+CREATE TABLE IF NOT EXISTS songplays (
 songplay_id bigint IDENTITY(1,1) PRIMARY KEY,
 start_time timestamp NOT NULL,
 user_id int NOT NULL,
@@ -83,18 +83,19 @@ song_table_create = ("""
 CREATE TABLE IF NOT EXISTS songs (
 song_id varchar PRIMARY KEY,
 title varchar NOT NULL,
-duration float NOT NULL,
-year int NOT NULL)
+artist_id varchar NOT NULL,
+year int NOT NULL,
+duration numeric NOT NULL)
 SORTKEY(song_id)
 """)
 
 artist_table_create = ("""
 CREATE TABLE IF NOT EXISTS artists (
 artist_id varchar NOT NULL,
-artist_latitude float,
-artist_longitude float,
+artist_name varchar NOT NULL,
 artist_location varchar NOT NULL,
-artist_name varchar NOT NULL)
+artist_latitude float,
+artist_longitude float)
 SORTKEY(artist_id)
 """)
 
@@ -133,36 +134,37 @@ REGION AS 'us-west-2'
 # FINAL TABLES
 
 songplay_table_insert = ("""
-INSERT INTO songplay(start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
-SELECT TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second' AS start_time, userid, level, song_id, artist_id, sessionid, location, useragent
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+SELECT DISTINCT TIMESTAMP 'epoch' + ts/1000 * INTERVAL '1 second' AS start_time, userid, level, song_id, artist_id, sessionid, location, useragent
 FROM staging_events AS se
 INNER JOIN staging_songs AS ss
 ON ss.artist_name = se.artist AND ss.title = se.song AND se.page = 'NextSong'
 """)
 
 user_table_insert = ("""
-INSERT INTO users
-SELECT userId, firstName, lastName, gender, level
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+SELECT DISTINCT userId, firstName, lastName, gender, level
 FROM staging_events
-WHERE userId IS NOT NULL AND page = 'NextSong'
+WHERE page = 'NextSong'
 """)
 
 song_table_insert = ("""
-INSERT INTO songs
-SELECT song_id, title, artist_id, year, duration
+INSERT INTO songs (song_id, title, artist_id, year, duration)
+SELECT DISTINCT song_id, title, artist_id, year, duration
 FROM staging_songs
 WHERE song_id IS NOT NULL
 """)
 
 artist_table_insert = ("""
-INSERT INTO artists
-SELECT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
+INSERT INTO artists (artist_id, artist_name, artist_location, artist_latitude, artist_longitude)
+SELECT DISTINCT artist_id, artist_name, artist_location, artist_latitude, artist_longitude
 FROM staging_songs
+WHERE song_id IS NOT NULL
 """)
 
 time_table_insert = ("""
-INSERT INTO time
-SELECT
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+SELECT DISTINCT 
 TIMESTAMP 'epoch' + (ts/1000) * INTERVAL '1 second' as start_time,
 EXTRACT(HOUR FROM start_time)  AS hour,
 EXTRACT(DAY FROM start_time)   AS day,
@@ -171,6 +173,7 @@ EXTRACT(MONTH FROM start_time) AS month,
 EXTRACT(YEAR FROM start_time)  AS year,
 to_char(start_time, 'Day') AS weekday
 FROM staging_events
+WHERE page = 'NextSong'
 """)
 
 # QUERY LISTS
